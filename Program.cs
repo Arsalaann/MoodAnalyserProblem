06181698565756f20a09895bac6e92f2c184fc02
Arsalaann <arsalankhanxk@gmail.com>
2023-05-27 18:04:45 +0530

    Refactor1.1

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ namespace MoodAnalyser
 	{
 		public static void Main(string[] args)
 		{
-			MoodAnalyzer person1=new MoodAnalyzer("");
+			MoodAnalyzer person1=new MoodAnalyzer("I am in sad mood");
 			if(person1.AnalyseMood()==true)
 				Console.WriteLine("Happy");
 			else
