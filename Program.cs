d4d6261acfefd7c6e49781273289331f147ca1ed
Arsalaann <arsalankhanxk@gmail.com>
2023-05-27 18:18:40 +0530

    try catch

--- Program.cs
+++ Program.cs
@@ -4,12 +4,27 @@ namespace MoodAnalyser
 	{
 		public static void Main(string[] args)
 		{
-			MoodAnalyzer person1=new MoodAnalyzer("I am in Happy mood");
-			if(person1.AnalyseMood()==true)
-				Console.WriteLine("Happy");
-			else
-				Console.WriteLine("Sad");
+			try
+			{
+				Console.Write("Enter mood status: ");
+				string mood="";
+				mood+=Console.ReadLine();
+				MoodAnalyzer person1=new MoodAnalyzer(mood);
+				if(person1.AnalyseMood()==true)
+					Console.WriteLine("Happy");
+				else
+					Console.WriteLine("Sad");
+			}
+			catch(FormatException)
+			{
+				Console.WriteLine("You have to enter a string");
+			}
+			catch(Exception exp2)
+			{
+				Console.WriteLine(exp2.Message);
+			}
 		}
+
 	}
 }
 
